70e6675bb169d27e4577918053a346af8c7eb39c
lichen354 <lichen354@163.com>
2017-05-06 08:49:34 +0800

    add quick sort

--- src/com/teach/array/Sort_.java
+++ src/com/teach/array/Sort_.java
@@ -1,4 +1,4 @@
-package com.etc.demo.array;
+package com.teach.array;
 
 public class Sort_ {
 	public static void main(String[] args) {
@@ -65,6 +65,37 @@ public class Sort_ {
 	 * 它跟我们牌扑克牌的方式相似。
 	 * 每步将一个待排序的对象,按其排序码大小,插入到前面已经排好序的一组对象的适当位置上,直到对象全部插入为止
 	 */
+	public static void insertSort(int[] arr){
+		int next;
+		int j;
+		for(int i = 1; i < arr.length; i++){
+			next = arr[i];
+			
+			for(j = i-1; j >= 0; j--){
+				if(next < arr[j]){
+					arr[j+1] = arr[j];
+				}else{
+					break;
+				}
+			}
+			arr[j+1] = next;
+		}
+	}
 	
-	
+	//while 实现插入排序
+		public void sortArrs2(int[] arrs){
+	        int len = arrs.length;
+	        int temp = 0;
+	        //进行len-1次循环，每次循环都将下标为i的元素插入到它前面已经排好序的队列中
+	        for(int i=1;i<len;i++){
+	        	if(arrs[i]<arrs[i-1]){
+	        		temp = arrs[i];
+	                while( i>0 && temp<arrs[i-1]){
+	                    arrs[i]=arrs[i-1];
+	                    i--;
+	                }
+	                 arrs[i] = temp;
+	            }
+	        }
+	    }
 }
