4152d008d1404233cdd951e7ec08b20899eb28ad
lichen354 <lichen354@163.com>
2017-05-09 22:04:04 +0800

    1

--- src/com/teach/io/K_MergeDemo.java
+++ src/com/teach/io/K_MergeDemo.java
@@ -1,14 +1,37 @@
-package com.etc.demo.io;
+package com.teach.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class K_MergeDemo {
 	public static void main(String[] args) {
 		
 	}
 	
-	
-	public static void merge(File fir){
-		
+	/**
+	 * 将多个文件合并到一个-----待测试
+	 * @throws IOException 
+	 */
+	public static void merge(File dir) throws IOException{
+		int fileNums = dir.list().length;
+		if(fileNums > 0){
+			FileInputStream[] fis = new FileInputStream[fileNums];
+			
+			FileOutputStream out = new FileOutputStream(new File("out"));
+			
+			for(int i = 0; i < fileNums; i++){
+				fis[i] = new FileInputStream(new File(dir, ++i + ".part"));
+				int len = 0;
+				byte[] buf = new byte[1024];
+				while((len = fis[i].read(buf)) != -1){
+					out.write(buf, 0, len);
+				}
+				fis[i].close();
+			}
+			out.close();
+		}
 	}
 }
